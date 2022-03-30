b3a187f118780ee942974d35376c170710964535
roberthdg <roberthdgg@gmail.com>
2022-03-30 18:23:10 -0500

    CORS config

--- src/server.js
+++ src/server.js
@@ -12,8 +12,14 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'../app/views'));
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
 //middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
